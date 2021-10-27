959b87b591ba895b3e33cca9ca8300d1dae74c90
Darshan29111998 <93201781+Darshan29111998@users.noreply.github.com>
2021-10-27 14:00:52 +0530

    Delete caching directory

--- caching/src/main/java/com/example/repository/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.model.Product;
-
-public interface ProductRepo extends CrudRepository<Product,Integer>{
-  public Product findProductNameByProductId(int id);
-}
